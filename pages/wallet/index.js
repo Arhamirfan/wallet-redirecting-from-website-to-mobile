68a369da6ea3c58a86962605d794a68b5e4afb6d
Arham Irfan <arham.irfan@logicon.com.pk>
2023-01-09 13:20:54 +0500

    test2

--- pages/wallet/index.js
+++ pages/wallet/index.js
@@ -2,11 +2,12 @@
 const wallet = () => {
     let walletConnection = () => {
         try {
+            // const deepLink = 'intent://scan/#Intent;package=io.metamask.metamask;scheme=metamask;end';
             const deepLink = 'intent://scan/#Intent;package=io.metamask.metamask;scheme=metamask;end';
             // const deepLink = 'itms-apps://itunes.apple.com/app/metamask/id1455363061';
             window.location.href = deepLink;
         } catch (e) {
-            console.log(e);
+            console.log('error:', e);
         }
     }
     return (
